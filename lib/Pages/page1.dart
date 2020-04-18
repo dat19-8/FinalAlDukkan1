5a4c15da227178efd2afafe49a66ef8ab088e85b
Ranin <Ranin@UPP000046513P>
2020-04-18 20:04:06 +0300

    updated

--- lib/Pages/page1.dart
+++ lib/Pages/page1.dart
@@ -9,31 +9,286 @@ import 'Vendor/vendcam.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:finaldukkan1/globals.dart';
 import 'page3.dart';
+import '../pages/Shopper/ShopApp/mainshop.dart';
+import '../Pages/Vendor/vendmainapp.dart';
+import 'package:better_uuid/uuid.dart';
+//class FirstRoute extends StatefulWidget {
+//  @override
+//  _FirstRouteState createState() => _FirstRouteState();
+//}
+//
+//class _FirstRouteState extends State<FirstRoute> {
+//  @override
+//  void initState() {
+//    checkuser();
+//  }
+//
+//  void checkuser() async {
+//    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
+//    if (user != null) {
+//      Navigator.pushReplacement(
+//          context, MaterialPageRoute(builder: (context) => ThirdPage()));
+//      print (user);
+//      print(phoneNumber);
+////    var documentt= Firestore.instance
+////        .collection('Vendors')
+////        .document(vendPhone).snapshots();
+////    builder: (context, snapshot){
+////    if (snapshot.data.)
+////    if (documentt=phoneNumber){
+////      print ("yes");
+////    }
+////    else {
+////      print ("no");
+////    }
+////    print (documentt);
+//
+//
+////      Firestore.instance
+////        .collection('Vendors')
+////        .document(vendPhone).get().
+////    then((DocumentSnapshot ds){
+////      // if (
+////     print(  ds.data['Orders']['Pinfo']['phone']);
+////     print ("hi");
+////         //== phoneNumber
+//////         {
+//////           Navigator.pushReplacement(
+//////        context, MaterialPageRoute(builder: (context) => ShopApp()));
+//////         }
+//////       else {
+//////         print ("no");
+////      });
+//////      });
+////      print ("hiiiiiiiiiii");
+//
+////      if( Firestore.instance.collection("Vendors").document(phoneNumber).snapshots() != null)
+////         {
+////           print ("hi");
+////           Navigator.pushReplacement(
+////               context, MaterialPageRoute(builder: (context) => Vendapp()));
+////           print (phoneNumber);
+////         }
+////
+////         else if( Firestore.instance.collection("Shoppers").document(phoneNumber).snapshots() != null){
+////        print ("hellooo");
+////           Navigator.pushReplacement(
+////       context, MaterialPageRoute(builder: (context) => ShopApp()));
+////
+////    }
+////      else
+////    {
+////
+////    Navigator.pushReplacement(
+////       context, MaterialPageRoute(builder: (context) => ThirdPage()));
+////    }
+//
+//
+//
+//
+//    } else {
+//      Navigator.pushReplacement(
+//          context, MaterialPageRoute(builder: (context) => PhonePage()));
+//    }
+//  }
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Color.fromRGBO(15, 76, 117, 100),
+//      appBar: AppBar(
+//        backgroundColor: Color.fromRGBO(27, 38, 44, 100),
+//        title: Center(
+//          child: Text(
+//            'الدكان',
+//            style: TextStyle(color: Colors.white, fontSize: 50.0),
+//          ),
+//        ),
+//      ),
+//      body: Column(
+//        children: <Widget>[
+//          Row(
+//            mainAxisAlignment: MainAxisAlignment.center,
+//            children: <Widget>[
+//              Container(
+//                margin: EdgeInsets.fromLTRB(0, 45.0, 0, 20.0),
+//                child: Image.asset("images/cart-1.png"),
+//                width: 200.0,
+//                height: 200.0,
+//              ),
+//            ],
+//          ),
+//          Row(
+//            mainAxisAlignment: MainAxisAlignment.center,
+//            children: <Widget>[
+//              Container(
+//                  margin: EdgeInsets.only(top: 70.0, bottom: 30.0),
+//                  width: MediaQuery.of(context).size.width * 0.65,
+//                  height: 50.0,
+//                  child: FlatButton(
+//                    color: Colors.green,
+//                    shape: new RoundedRectangleBorder(
+//                      borderRadius: new BorderRadius.circular(30.0),
+//                    ),
+//                    child: Text(
+//                      'أدخل رقم هاتفك',
+//                      style: TextStyle(color: Colors.white ),
+//                    ),
+//                    onPressed: () {
+////                      print("Phone number");
+//                      // Navigator.pushReplacement(context,
+//                      //     MaterialPageRoute(builder: (context) => PhonePage()));
+//                    },
+//                  )),
+//            ],
+//          ),
+//          Row(
+//            mainAxisAlignment: MainAxisAlignment.center,
+//            children: <Widget>[
+//              Container(
+//                  margin: EdgeInsets.only(top: 20.0),
+//                  child: FlatButton(
+//                    color: Colors.green,
+//                    shape: new RoundedRectangleBorder(
+//                      borderRadius: new BorderRadius.circular(30.0),
+//                    ),
+//                    child: Text(
+//                      'مشترك',
+//                      style: TextStyle(color: Colors.white),
+//                    ),
+//                    onPressed: () {
+//                      print("vend app");
+//                      Navigator.push(
+//                        context,
+//                        // MaterialPageRoute(builder: (context) => main()),
+//                        MaterialPageRoute(builder: (context) => CameraTab()),
+//                        //
+//                      );
+//                    },
+//                  ))
+//            ],
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//}
+
 
 class FirstRoute extends StatefulWidget {
+
   @override
   _FirstRouteState createState() => _FirstRouteState();
+
+
+
+
 }
 
 class _FirstRouteState extends State<FirstRoute> {
+
   @override
   void initState() {
+//    print (phoneNumber);
     checkuser();
   }
+  void  checkuser() async {
+    final FirebaseUser user =  await FirebaseAuth.instance.currentUser();
+    if (user !=null){
+      StreamBuilder(
+          stream: Firestore.instance
+              .collection('Vendors')
+              .document('00000001')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(child: Text('No data in DB '));
+            else {
+              if (snapshot.data['Products'].length == 0) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                        child: Text(
+                          "You have no Products",
+                          style: TextStyle(fontSize: 30.0),
+                        )),
+                  ],
+                );
+              } else {
+                print("theres data");
+              }
+          }
+      );
+//      var phonee= user.phoneNumber;
+//      var newphone=phonee.substring(4);
+//      String x="12";
+//
+//      print(x);
+//print (user.phoneNumber);
 
-  void checkuser() async {
-    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
-    if (user != null) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => ThirdPage()));
-      print(user);
-      print(phoneNumber);
-      
-    } else {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => PhonePage()));
+//    print(phoneNumber);
+//      Navigator.pushReplacement(
+//          context,
+//          MaterialPageRoute(
+//              builder: (context) =>ThirdPage())
+//      );
+//      print(user);
+//
+//
+
+//           Navigator.pushReplacement(
+//               context, MaterialPageRoute(builder: (context) => MainVend()));
+//
+//           print (user.phoneNumber);
+//         }
+//
+//         else if( (Firestore.instance.collection("Shopers").document("user.phoneNumber").snapshots())!null){
+//        print ("hellooo");
+//           Navigator.pushReplacement(
+//       context, MaterialPageRoute(builder: (context) => MainShop()));
+//
+//    }
+//      else
+//    {
+//      print("hello");
+//
+//    Navigator.pushReplacement(
+//       context, MaterialPageRoute(builder: (context) => ThirdPage()));
+  //  }
+
+
+//StreamBuilder(
+//  stream: Firestore.instance.collection("Shoppers").snapshots(),
+//  builder:(conext,snapshot){
+////  print(snapshot.data.documents["+96176080604"]['Pinfo']);
+//
+//
+//  }
+//);
+//    print (user.phoneNumber);
+//    StreamBuilder(
+//        stream: Firestore.instance
+//            .collection('Shoppers')
+//            .document(user.phoneNumber)
+//            .snapshots(),
+//        builder: (context, snapshot) {
+//          if (!snapshot.hasData)
+//            print('No data in DB ');
+//          else
+//            print ("data");
+
+//        });
+
+
+    }
+    else{
+      print ("yes");
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => PhonePage()));
     }
   }
+    var shopper_found = false;
+    var vendor_founf = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +304,7 @@ class _FirstRouteState extends State<FirstRoute> {
       ),
       body: Column(
         children: <Widget>[
-          Row(
+         streambuilder Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
@@ -77,9 +332,10 @@ class _FirstRouteState extends State<FirstRoute> {
                       style: TextStyle(color: Colors.white ),
                     ),
                     onPressed: () {
-                      print("Phone number");
-                      // Navigator.pushReplacement(context,
-                      //     MaterialPageRoute(builder: (context) => PhonePage()));
+//                      print("Phone number");
+                    print (phoneNumber);
+//                       Navigator.pushReplacement(context,
+//                           MaterialPageRoute(builder: (context) => PhonePage()));
                     },
                   )),
             ],
@@ -115,125 +371,3 @@ class _FirstRouteState extends State<FirstRoute> {
     );
   }
 }
-
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-// import 'package:finaldukkan1/globals.dart';
-// import '../Pages/phone.dart';
-// import 'Shopper/available.dart';
-// import 'Vendor/vendmainapp.dart';
-// import 'phone.dart';
-// import 'page3.dart';
-
-// class FirstRoute extends StatefulWidget {
-//   @override
-//   _FirstRouteState createState() => _FirstRouteState();
-// }
-
-// class _FirstRouteState extends State<FirstRoute> {
-//   @override
-//   void initState() {
-//     checkuser();
-//   }
-
-//   void checkuser() async {
-//     final FirebaseUser user = await FirebaseAuth.instance.currentUser();
-//     if (user != null) {
-//       Navigator.pushReplacement(
-//           context, MaterialPageRoute(builder: (context) => ThirdPage()));
-//       print(user);
-//       print(phoneNumber);
-//     } else {
-//       Navigator.pushReplacement(
-//           context, MaterialPageRoute(builder: (context) => PhonePage()));
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color.fromRGBO(15, 76, 117, 100),
-//       appBar: AppBar(
-//         backgroundColor: Color.fromRGBO(27, 38, 44, 100),
-//         title: Center(
-//           child: Text(
-//             'الدكان',
-//             style: TextStyle(color: Colors.white, fontSize: 50.0),
-//           ),
-//         ),
-//       ),
-//       body: Column(
-//         children: <Widget>[
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               Container(
-//                 margin: EdgeInsets.fromLTRB(0, 45.0, 0, 20.0),
-//                 child: Image.asset("images/cart-1.png"),
-//                 width: 200.0,
-//                 height: 200.0,
-//               ),
-//             ],
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               Container(
-//                   margin: EdgeInsets.only(top: 70.0, bottom: 30.0),
-//                   width: MediaQuery.of(context).size.width * 0.65,
-//                   height: 50.0,
-//                   child: FlatButton(
-//                     color: Colors.green,
-//                     shape: new RoundedRectangleBorder(
-//                       borderRadius: new BorderRadius.circular(30.0),
-//                     ),
-//                     child: Text(
-//                       'أدخل رقم هاتفك',
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                     onPressed: () {
-//                       //checkuser();
-//                       print("Phone number");
-
-//                       // Navigator.pushReplacement(context,
-//                       //     MaterialPageRoute(builder: (context) => PhonePage()));
-//                     },
-//                   )),
-//             ],
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               Container(
-//                   margin: EdgeInsets.only(top: 20.0),
-//                   child: FlatButton(
-//                     color: Colors.green,
-//                     shape: new RoundedRectangleBorder(
-//                       borderRadius: new BorderRadius.circular(30.0),
-//                     ),
-//                     child: Text(
-//                       'مشترك',
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                     onPressed: () {
-//                       print("vend app");
-//                       Navigator.push(
-//                         context,
-//                         MaterialPageRoute(builder: (context) => MainVend()),
-//                       );
-//                     },
-//                   ))
-//             ],
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// @override
-// Widget build(BuildContext context) {
-//   // TODO: implement build
-//   return null;
-// }
--- lib/Pages/phone.dart
+++ lib/Pages/phone.dart
@@ -4,12 +4,13 @@ import 'package:rflutter_alert/rflutter_alert.dart';
 import '../Pages/page3.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class PhonePage extends StatelessWidget {
+class PhonePage extends StatelessWidget{
+
   String _smsVerificationCode;
-  
-  
-    _verifyPhoneNumber(BuildContext context) async {
-     phoneNumber = "+961" + _phoneNumberController.text.toString();
+
+
+  _verifyPhoneNumber(BuildContext context) async {
+    phoneNumber = "+961" + _phoneNumberController.text;
     final FirebaseAuth _auth = FirebaseAuth.instance;
     await _auth.verifyPhoneNumber(
         phoneNumber: phoneNumber,
@@ -20,19 +21,19 @@ class PhonePage extends StatelessWidget {
         // called when the SMS code is sent
         codeSent: (verificationId, [code]) => _smsCodeSent(verificationId, [code]));
   }
-    _verificationComplete(AuthCredential authCredential, BuildContext context) {
+  _verificationComplete(AuthCredential authCredential, BuildContext context) {
     FirebaseAuth.instance.signInWithCredential(authCredential).then((authResult) {
       final snackBar = SnackBar(content: Text("Success!!! UUID is: " + authResult.user.uid));
       Scaffold.of(context).showSnackBar(snackBar);
 
     });
-    
 
+//print (phoneNumber);
     Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>ThirdPage())
-                                    );
+        context,
+        MaterialPageRoute(
+            builder: (context) =>ThirdPage())
+    );
   }
 
   _smsCodeSent(String verificationId, List<int> code) {
@@ -43,21 +44,26 @@ class PhonePage extends StatelessWidget {
   _verificationFailed(AuthException authException, BuildContext context) {
     final snackBar = SnackBar(content: Text("Exception!! message:" + authException.message.toString()));
     Scaffold.of(context).showSnackBar(snackBar);
-    
+
   }
 
   _codeAutoRetrievalTimeout(String verificationId) {
     // set the verification code so that we can use it to log the user in
     _smsVerificationCode = verificationId;
-   
+
   }
 
   final TextEditingController _phoneNumberController =
-      new TextEditingController();
+  new TextEditingController();
+
+
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      resizeToAvoidBottomPadding: true,
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Color.fromRGBO(15, 76, 117, 100),
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(27, 38, 44, 100),
@@ -84,6 +90,7 @@ class PhonePage extends StatelessWidget {
                   child: Text(
                     "أدخل رقم هاتفك",
                     style: TextStyle(fontSize: 20.0, color: Colors.white),
+
                   ),
                 ),
                 Container(
@@ -130,20 +137,24 @@ class PhonePage extends StatelessWidget {
                         onPressed: () {
                           if (_phoneNumberController.text.length != 8) {
                             Alert(
-                                    context: context,
-                                    title: 'رقم الهاتف غير صحيح')
+                                context: context,
+                                title: 'رقم الهاتف غير صحيح')
                                 .show();
                           } else {
-                            tempPhone = _phoneNumberController.text;
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ThirdPage()));
-                            
-                            //  _verifyPhoneNumber(context);
-                            //  print(phoneNumber);
+                            tempPhone = "+961" + _phoneNumberController.text;
+
+                            // Navigator.pushReplacement(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (context) =>Phoneauth())
+                            //         );
+
+                            _verifyPhoneNumber(context);
+//                            print(phoneNumber);
+
                           }
                         },
+
                         padding: EdgeInsets.all(16.0),
                       ),
                     ),
@@ -155,176 +166,3 @@ class PhonePage extends StatelessWidget {
     );
   }
 }
-
-// import 'package:finaldukkan1/globals.dart';
-// import 'package:flutter/material.dart';
-// import 'package:rflutter_alert/rflutter_alert.dart';
-// import '../Pages/page3.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'page3.dart';
-
-// // class MyHomePage extends StatefulWidget {
-// //   @override
-// //   PhonePage createState() => new PhonePage();
-// // }
-
-// class PhonePage extends StatelessWidget {
-//   String _smsVerificationCode;
-
-//   _verifyPhoneNumber(BuildContext context) async {
-//     print("before  _verifyPhoneNumber");
-//     phoneNumber = "+961" + _phoneNumberController.text.toString();
-//     print("after  _verifyPhoneNumber");
-//     final FirebaseAuth _auth = FirebaseAuth.instance;
-//     await _auth.verifyPhoneNumber(
-//         phoneNumber: phoneNumber,
-//         timeout: Duration(seconds: 5),
-//         verificationCompleted: (authCredential) =>
-//             _verificationComplete(authCredential, context),
-//         verificationFailed: (authException) =>
-//             _verificationFailed(authException, context),
-//         codeAutoRetrievalTimeout: (verificationId) =>
-//             _codeAutoRetrievalTimeout(verificationId),
-//         // called when the SMS code is sent
-//         codeSent: (verificationId, [code]) =>
-//             _smsCodeSent(verificationId, [code]));
-//     print("after auth.verifyPhone");
-//   }
-
-//   _verificationComplete(AuthCredential authCredential, BuildContext context) {
-//     print("1");
-//     FirebaseAuth.instance
-//         .signInWithCredential(authCredential)
-//         .then((authResult) {
-//       final snackBar =
-//           SnackBar(content: Text("Success!!! UUID is: " + authResult.user.uid));
-//       Scaffold.of(context).showSnackBar(snackBar);
-//     });
-
-//     Navigator.pushReplacement(
-//         context, MaterialPageRoute(builder: (context) => ThirdPage()));
-//   }
-
-//   _smsCodeSent(String verificationId, List<int> code) {
-//     // set the verification code so that we can use it to log the user in
-//     _smsVerificationCode = verificationId;
-//   }
-
-//   _verificationFailed(AuthException authException, BuildContext context) {
-//     final snackBar = SnackBar(
-//         content:
-//             Text("Exception!! message:" + authException.message.toString()));
-//     Scaffold.of(context).showSnackBar(snackBar);
-//   }
-
-//   _codeAutoRetrievalTimeout(String verificationId) {
-//     // set the verification code so that we can use it to log the user in
-//     _smsVerificationCode = verificationId;
-//   }
-
-//   final TextEditingController _phoneNumberController =
-//       new TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       resizeToAvoidBottomPadding: false,
-//       backgroundColor: Color.fromRGBO(15, 76, 117, 100),
-//       appBar: AppBar(
-//         backgroundColor: Color.fromRGBO(27, 38, 44, 100),
-//         title: Center(
-//           child: Text(
-//             'الدكان',
-//             style: TextStyle(color: Colors.white, fontSize: 50.0),
-//           ),
-//         ),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Center(
-//           child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 Container(
-//                   margin: EdgeInsets.fromLTRB(0, 0, 0, 10.0),
-//                   child: Image.asset("images/cart-1.png"),
-//                   width: 200.0,
-//                   height: 200.0,
-//                 ),
-//                 Container(
-//                   padding: EdgeInsets.all(20.0),
-//                   child: Text(
-//                     "أدخل رقم هاتفك",
-//                     style: TextStyle(fontSize: 20.0, color: Colors.white),
-//                   ),
-//                 ),
-//                 Container(
-//                   width: MediaQuery.of(context).size.width * 0.65,
-//                   height: 50.0,
-//                   child: TextFormField(
-//                     keyboardType: TextInputType.number,
-//                     controller: _phoneNumberController,
-//                     cursorColor: Colors.white,
-//                     autofocus: true,
-//                     style: TextStyle(color: Colors.white),
-//                     decoration: InputDecoration(
-//                       prefix: Container(
-//                         padding: EdgeInsets.all(4.0),
-//                         child: Text(
-//                           "+961",
-//                           style: TextStyle(
-//                               color: Colors.white, fontWeight: FontWeight.bold),
-//                         ),
-//                       ),
-//                     ),
-//                     autovalidate: true,
-//                     autocorrect: false,
-//                     maxLengthEnforced: true,
-//                   ),
-//                 ),
-//                 Container(
-//                   padding: EdgeInsets.all(16),
-//                   width: MediaQuery.of(context).size.width * 0.55,
-//                   height: 175.0,
-//                   child: Center(
-//                     child: SizedBox(
-//                       width: MediaQuery.of(context).size.width * 0.85,
-//                       child: RaisedButton(
-//                         shape: RoundedRectangleBorder(
-//                             borderRadius: BorderRadius.circular(0.0)),
-//                         child: Text(
-//                           "واصل",
-//                           style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 18.0,
-//                               fontWeight: FontWeight.bold),
-//                         ),
-//                         onPressed: () {
-//                           if (_phoneNumberController.text.length != 8) {
-//                             Alert(
-//                                     context: context,
-//                                     title: 'رقم الهاتف غير صحيح')
-//                                 .show();
-//                           } else {
-//                             // tempPhone = _phoneNumberController.text;
-
-//                             // Navigator.pushReplacement(
-//                             //     context,
-//                             //     MaterialPageRoute(
-//                             //         builder: (context) =>Phoneauth())
-//                             //         );
-
-//                             _verifyPhoneNumber(context);
-//                             print(phoneNumber);
-//                           }
-//                         },
-//                         padding: EdgeInsets.all(16.0),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ]),
-//         ),
-//       ),
-//     );
-//   }
-// }
